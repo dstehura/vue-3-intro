3778bc7cd6f77d63739f6dac0105837f038ea7b0
David Stehura <david.stehura@gmail.com>
2024-05-28 15:55:41 -0400

    End of lesson 3- attribute binding

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.amazon.com/s?k=socks',
             description: 'A pair of warm, fuzzy socks',
         }
     }
