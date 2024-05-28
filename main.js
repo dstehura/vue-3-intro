4aab4994ae5b5d1aca9ee14515f7d354f6d3d20e
David Stehura <david.stehura@gmail.com>
2024-05-28 16:58:57 -0400

    end of lesson 4- conditional rendering

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
+            inventory: 100,
+            onSale: true,
             url: 'https://www.amazon.com/s?k=socks',
             description: 'A pair of warm, fuzzy socks',
         }
