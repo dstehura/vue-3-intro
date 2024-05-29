4857682d2831fc8bd8c4151e8459aebe8061c7aa
David Stehura <david.stehura@gmail.com>
2024-05-28 21:01:58 -0400

    end L6- event handling

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inventory: 0,
@@ -9,10 +10,26 @@ const app = Vue.createApp({
             description: 'A pair of warm, fuzzy socks',
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green'},
-                { id: 2235, color: 'blue'}
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', inventory: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', inventory: 0 }
             ],
             sizes: ['S', 'M', 'L', 'XL'],
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        removeFromCart() {
+            if (this.cart > 0) {
+                this.cart -= 1
+            }
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
+        },
+        updateInventory(inventory) {
+            this.inventory = inventory
         }
     }
 })
